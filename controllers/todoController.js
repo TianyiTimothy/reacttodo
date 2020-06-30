801a697556c27b75809b6c964221bc2735555267
TimothyZhang <TimothyZhang@foxmail.com>
2020-07-01 03:00:06 +0800

    init

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -4,7 +4,9 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://hfpp2012:hfpp2012@ds151068.mlab.com:51068/todos');
+// mongoose.connect('mongodb://hfpp2012:hfpp2012@ds151068.mlab.com:51068/todos');
+// mongoose.connect('mongodb+srv://root:root@cluster0.xbngn.mongodb.net/<dbname>?retryWrites=true&w=majority');
+mongoose.connect('mongodb://root:root@cluster0-shard-00-00.xbngn.mongodb.net:27017,cluster0-shard-00-01.xbngn.mongodb.net:27017,cluster0-shard-00-02.xbngn.mongodb.net:27017/<dbname>?ssl=true&replicaSet=atlas-evj9l3-shard-0&authSource=admin&retryWrites=true&w=majority');
 
 var todoSchema = new mongoose.Schema({
   item: String
